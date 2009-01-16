07390cf82f3a50af4472c88581c0bfccf0ca59e3
Lloyd Hilaiel <lloyd@hilaiel.com>
2009-01-15 18:04:06 -0700

    initial sketch

---
+++ src/downloader.rb
@@ -0,0 +1,79 @@
+require 'Pathname'
+ 
+
+class Downloader
+  def initialize(args)
+    @tempDir = args["temp_dir"]
+    if CONFIG['arch'] =~ /mswin/
+      @platform = "Windows"
+    else
+      @platform = "Darwin"
+    end
+    @fileNumber = 0
+  end
+
+  def get(bp, args)
+    url = args['url']
+    perms = @platform == "Windows" ? "wb" : "w"
+    path = @tempDir + "/" + "downloaded_" + @fileNumber
+    f = File.new(path, perms)
+
+    totalSize = 0
+    lastPercent = 0
+
+    f.write(open(url,
+                 :content_length_proc => lambda {|t|
+                   if (t && t > 0)
+                     totalSize = t
+                   end
+                 },
+                 :progress_proc => lambda {|s|
+                   if (totalSize > 0)
+                     percent = ((s.to_f / totalSize) * 100).to_i
+                     if (percent/interval > lastPercent/interval)
+                       lastPercent = percent
+                       if args.include? "callback"
+                         args["callback"].invoke(percent)
+                       end
+                     end
+                   end
+                 }).read)
+    f.close()
+    
+    bp.complete(Pathname.new(path))
+  end
+end
+
+rubyCoreletDefinition = {
+  'class' => "Downloader",
+  'name'  => "Downloader",
+  'major_version' => 0,
+  'minor_version' => 0,
+  'micro_version' => 1,
+  'documentation' => 
+    'Download remote files over HTTP to a temporary location and return file handles to javascript.',
+
+  'functions' =>
+  [
+    {
+      'name' => 'get',
+      'documentation' => "Accepts a url, returns a BrowserPlus file handle.",
+      'arguments' =>
+      [
+         {
+            'name' => 'url',
+            'type' => 'string',
+            'required' => true,
+            'documentation' => 'the url to the content to download.'
+          },
+          {
+            'name' => 'callback',
+            'type' => 'callback',
+            'required' => false,
+            'documentation' => 'download progress'
+          }
+      ]
+    }
+  ] 
+}
+
